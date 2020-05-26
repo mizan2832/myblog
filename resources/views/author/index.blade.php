f605f5b025bbd765cca8401fe970b779444a6e20
mizan2832 <programmer620@gmail.com>
2020-05-26 14:13:05 +0600

    first commit

---
+++ resources/views/author/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog </title>
+    <link rel="stylesheet" href="{{ asset('front/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/css/style.css') }}">
+    {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> --}}
+    {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> --}}
+
+</head>
+<body>
+    <div>
+        <div class="container">
+         
+            
+                <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+                    <a class="navbar-brand" href="#">Logo</a>
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                      </li>
+                    </ul>
+                  </nav>
+                  
+            
+        
+            
+              
+              <div style="margin-top:80px">
+                <h3>Top Fixed Navbar</h3>
+                <p>A fixed navigation bar stays visible in a fixed position (top or bottom) independent of the page scroll.</p>
+                <h1>Scroll this page to see the effect</h1>
+              </div>
+        </div>
+    </div>
+</body>
+</html>
